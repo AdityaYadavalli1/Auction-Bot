2f8fa9b0339766124b8a83aafd9f97c807dfac90
AdityaYadavalli1 <aditya.yadavalli@yahoo.com>
2021-04-10 19:32:40 +0530

    Fix MyBot

--- bolt-v1/my_bots/mybot.py
+++ bolt-v1/my_bots/mybot.py
@@ -9,15 +9,24 @@ class MyBot(Client):
         self.name = "Amla Thopu Dammu Unte Aapu"  # Your Bot's Name
         # Your Initialization Code Here
         self.s = 0.00001
+        self.e = 0
         self.last_bid = {}
         pass
 
     async def start(self, auction_id):
+        # self.e = 0
         await super().start(auction_id)
         self.last_bid[auction_id] = self.s
         print("Start Bid")
         await super().submit_bid(auction_id, self.s)
-
+        while True:
+            curr_bid = self.last_bid[auction_id]
+            next_bid = max(1.125*curr_bid, self.e)
+            if self.utility(next_bid) > self.utility(curr_bid):
+                self.last_bid[auction_id] = next_bid+0.00001
+                await super().submit_bid(auction_id, next_bid+0.00001)
+            else:
+                await super().end_auction(auction_id)  
         # Your code for starting an auction
 
     @staticmethod
@@ -32,15 +41,8 @@ class MyBot(Client):
     async def receive_bid(self, auction_id, bid_value):
         # code for receiving bid
         await super().receive_bid(auction_id, bid_value)
-        curr_bid = self.last_bid[auction_id]
-        next_bid = max(1.125*curr_bid, bid_value)
-        if self.utility(next_bid) > self.utility(curr_bid):
-            await asyncio.sleep(1)
-            self.last_bid[auction_id] = next_bid+0.00001
-            await super().submit_bid(auction_id, next_bid+0.00001)
-        else:
-            await asyncio.sleep(1)
-            await super().end_auction(auction_id)
+        if bid_value > self.e:
+            self.e = bid_value
 
     async def end_auction(self, auction_id):
         await super().end_auction(auction_id)
